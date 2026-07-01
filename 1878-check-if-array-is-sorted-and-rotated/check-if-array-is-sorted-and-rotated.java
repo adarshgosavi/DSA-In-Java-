22d0dddeef31c90417b45c0f00c6a56f11e1155d
adarshgosavi <aadrshgosavi89@gmail.com>
2026-07-01 23:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (77.23%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean check(int[] nums) {
         int dup = 0;
-
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > nums[(i + 1) % nums.length]){
                 dup++;
